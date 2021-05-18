a8c126e5f6fc57c71f6f1fd044345a2aec74713b
uwaisalqarni <qarniuwaisal565@gmail.com>
2021-05-19 05:07:04 +0700

    First Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1dd0628034a39983d5ea1011ca55dffe'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f55c0bb4-d33d-4e55-a453-36bfe5768ae6'
+};
